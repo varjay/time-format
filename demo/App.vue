a8bca7043005c36de6fe8d0b4f0d4b34d9c8184f
goudan <goudan@baidu.com>
2019-01-23 18:39:17 +0800

    update readme.

--- demo/App.vue
+++ demo/App.vue
@@ -1,8 +1,22 @@
 <template>
   <div id="app">
-    获取时间：2018-07-17 14:26<br>
+    传入日期：2018-07-17 14:26<br>
     详细模式：{{test('detail', '2018-07-17 14:26')}}<br>
     简短模式：{{test('short', '2018-07-17 14:26')}}
+    <br>
+    <br>
+    传入时间戳：{{now}}(当前时间)
+    <br>
+    简短模式：{{test('short', now)}}
+    <br>
+    详细模式：{{test('detail', now)}}
+    <br>
+    <br>
+    传入时间戳：1546275661000（2019年01月01日 01时01分01秒）
+    <br>
+    简短模式：{{test('short', 1546275661000)}}
+    <br>
+    详细模式：{{test('detail', 1546275661000)}}
   </div>
 </template>
 
@@ -12,6 +26,7 @@ window.TimeFormat = TimeFormat
 export default {
   data() {
     return {
+      now: new Date().getTime(),
     }
   },
   methods: {
